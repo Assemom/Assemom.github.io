5920ca6ab2c453170e70192477ae435a5d2ba623
assem omar <assemomar202@gmail.com>
2026-06-08 22:42:41 +0300

    Deploy portfolio

---
+++ _next/static/chunks/0jpiqk38atcje.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,35556,t=>{t.v(c=>Promise.all(["static/chunks/1q7cb-s1edw5q.js"].map(c=>t.l(c))).then(()=>c(15238)))}]);
